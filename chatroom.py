e0ff8315208ae006bfee73d2ae1a526326218fbc
David Doyle <ddoyle3@tcd.ie>
2016-11-07 13:45:21 +0000

    adding some thread safety structures

--- chatroom.py
+++ chatroom.py
@@ -1,5 +1,6 @@
-import datetime
 from threading import Condition
+import datetime
+import threading
 
 
 
@@ -8,12 +9,9 @@ class chatroom_manager:
     def __init__(self):
         self.rooms = {}
         self.room_count = 0
+        self.manager_lock = Lock()
 
     def add_new_message(self, room_name, client_handle, client_id, client_msg_string):
-        if room_name not in self.rooms:
-            new_room = chatroom(room_name, self.room_count)
-            self.room_count += 1
-
         self.rooms[room_name].add_new_message(client_handle, client_id, client_msg_string)
         
     def get_new_messages(self, room_name, starting_id):
@@ -25,6 +23,13 @@ class chatroom_manager:
         Alerts room of new member. Returns tuple containing room reference and room condition
         for the client to wait on
         """
+        #if room doesn't exist - create it
+        self.manager_lock.acquire()
+        if room_name not in self.rooms:
+            self.rooms[room_name] = chatroom(room_name, self.room_count)
+            self.room_count += 1
+        self.manager_lock.release()
+
         join_message = "--|>%s has joined the room, say hello guys!"%client_handle
         room = self.rooms[room_name]
         room.add_new_message(client_handle, client_id, client_msg_string)
@@ -41,17 +46,38 @@ class chatroom:
 
 
     def add_new_message(self, client_handle, client_id, client_msg_string)
+        """
+        Adds a new message to the chat room - operation is atomic
+        """
         new_message = message(client_handle, client_id, client_msg_string, self.room_record_count)
+
+        self.room_condition.acquire()       #SAFE SECTION START#
+
         self.room_record.append(new_message)
         self.room_record_count += 1
 
+        self.room_condition.release()       #SAFE SECTION END#
+
+
     def get_new_messages(self, starting_id)
         """
-        Returns a list of messages from the starting_id to the current count
+        Returns a list of messages from the starting_id to the current count 
+        operation is atomic
+        If starting_id is greater than the current room_record_count this will block
+        until new messages arrive
         """
         messages = []
+
+        self.room_condition_acquire()       #SAFE SECTION START#
+        
+        #will wait here until starting_id < room_record_count - as notified
+        while starting_id > self.room_record_count:
+            self.room_condition.wait()
+
         for i in range(i, len(self.room_record_count)):
             messages.append(self.room_record[i])
+
+        self.room_condition.release()       #SAFE SECTION END#
 
         return messages
 
@@ -62,3 +88,5 @@ class message:
         self.client_msg_value = client_msg_string
         self.client_msg_time = datetime.datetime.now()
         self.client_msg_id = count
+
+
